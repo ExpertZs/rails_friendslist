44e9f659ada8a7b9ec97351706bdbf4b97d669ac
ExpertZs <zs.freelancer1@gmail.com>
2022-09-27 23:25:34 +0600

    Associated friends with User

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -65,6 +65,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :facebook)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :facebook, :user_id)
     end
 end
